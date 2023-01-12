90716052d282cb6256739f2a242d66e9169f5a18
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-01-12 11:35:39 +0100

    Update and rename my_strchr.c to 5_my_strchr.c

--- 5_my_strchr.c
+++ 5_my_strchr.c
@@ -6,7 +6,7 @@ char* my_strchr(char* param_1, char param_2)
     {
         if(param_1[i] == param_2)
         {
-            return param_1 + i  ;
+            return param_1 + i;
         }
     }
     
