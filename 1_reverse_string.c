20f5742d911d0a88db140db2a4715fbd864a37d2
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-07-12 13:13:10 +0200

    Update 1_reverse_string.c

--- 1_reverse_string.c
+++ 1_reverse_string.c
@@ -2,25 +2,19 @@
 
 char* reverse_string(char* param_1)
 {
-   //size param_1
-   int n = strlen(param_1);
-   n--; 
-   
-   char* newStr;
-   int index = 0;
-   
-   //no string given as parameter
-   if(*param_1 == 0)
-   {
+    int len = strlen(param_1) - 1; 
+    char *newStr = malloc(sizeof(char) * (len + 1));
+    int index = 0;
+
+    if(param_1 == NULL)
+    {
         return param_1;
-   }
-   
-   //iterate from the end through param_1 
-   for(int i = n; param_1[i] != '\0'; --i) 
-   {
-      newStr[index++] = param_1[i];
-      newStr[index] = '\0';
-   }
-   
-   return newStr;
+    }
+
+    for (int i = len; i >= 0; i--) 
+    {
+        newStr[index++] = param_1[i];
+    }
+
+    return newStr;
 }
